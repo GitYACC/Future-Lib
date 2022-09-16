acd724d5fa545f20a5164f1440e7852fd7bf72c1
GitYACC <shastrysamarth@gmail.com>
2022-09-16 15:54:39 -0700

    discord won't let me publish a repository with my actual token, so I had to encode it lmfao

--- env/run.dev.py
+++ env/run.dev.py
@@ -2,16 +2,19 @@ import lightbulb
 import hikari
 from dataclasses import dataclass
 from future.discord.embed import BaseEmbed
+from base64 import urlsafe_b64decode
 
 @dataclass
 class TestServer:
-    client_secret: str = "NzI0MDAzMjU1NDIzODYwNzY5.Xu52kw.5WPfqBkpqTPfk_qs6fai0xtdv4A"
+    client_secret: bytes = b"TnpJME1EQXpNalUxTkRJek9EWXdOelk1LkdmbkFoVi51UDVRVGJMVW95TDl6VExHV2djVFF5LUM3YlI1M1FsOGZzWndUMA=="
     id: int = 724002247360380979
 
 
 bot = lightbulb.BotApp(
     intents=hikari.Intents.ALL,
-    token=TestServer.client_secret,
+    token=urlsafe_b64decode(
+        TestServer.client_secret
+    ),
     default_enabled_guilds=TestServer.id,
     banner=None
 )
